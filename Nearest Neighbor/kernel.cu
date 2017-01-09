8f6c909265835060d36541c0a4057be1273885d9
mGrondzi <manuelgrondziel.gr@gmail.com>
2017-01-09 16:15:19 +0100

    NN on GPU

--- Nearest Neighbor/kernel.cu
+++ Nearest Neighbor/kernel.cu
@@ -4,15 +4,29 @@
 
 #include <stdio.h>
 #include <cstdio>
+#include <iostream>
+#include <IL/il.h>
+#include "image.h"
+
+#define FACTOR 4
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+/*__global__ void addKernel(int *c, const int *a, const int *b)
 {
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 
 	// Here our shit.
+}*/
+
+__global__ void nearest_neighbor(uint32_t *dp, uint32_t *sp, ILuint height, ILuint width, int factor)
+{
+	for (int row = 0; row < height; ++row) {
+		for (int col = 0; col < width; ++col) {
+			dp[(row * width + col)] = sp[((row / factor) * width/factor + (col / factor))];
+		}
+	}
 }
 
 extern void CallNearestNeighbor();
@@ -20,127 +34,60 @@ extern void CallNearestNeighbor();
 int main()
 {
 
-	CallNearestNeighbor();
+	//CallNearestNeighbor();
+	ILuint handle;
+	ilInit();
+	ilEnable(IL_ORIGIN_SET);
+	ilGenImages(1, &handle);
+	ilBindImage(handle);
+	cudaError_t cudaStatus;
 
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-	int deviceCount;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	std::cout << "Read images" << std::endl;
+	Image *org = new Image("image.png");
+	Image *res = new Image("image3.png", org->getWidth() * FACTOR, org->getHeight() * FACTOR);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	std::cout << "Execute nearest neighbor algorithm" << std::endl;
+	
+	// prepare
+	uint32_t *input, *out, *temp;
+	cudaStatus = cudaSetDevice(0);
+	cudaMalloc(&input, org->getHeight() * org->getWidth() * sizeof(uint32_t));
+	cudaMalloc(&out, res->getHeight() * res->getWidth() * sizeof(uint32_t));
+	
+	cudaMemcpy(input, org->getData(), org->getHeight() * org->getWidth() * sizeof(uint32_t), cudaMemcpyHostToDevice);
+	
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-	cudaGetDeviceCount(&deviceCount);
-	int device;
-	for (device = 0; device < deviceCount; ++device) {
-		cudaDeviceProp deviceProp;
-
-		cudaGetDeviceProperties(&deviceProp, device);
-		// Choose which GPU to run on, change this on a multi-GPU system.
-		cudaStatus = cudaSetDevice(device);
-		printf("Device %d has compute capability %d.%d. -  %d\n",
-			device, deviceProp.major, deviceProp.minor, cudaStatus);
-		printf("multiProcessorCount %d\n", deviceProp.multiProcessorCount);
-		printf("maxThreadsPerMultiProcessor %d\n", deviceProp.maxThreadsPerMultiProcessor);
-		printf("Max Threads %d", deviceProp.multiProcessorCount * deviceProp.maxThreadsPerMultiProcessor);
+	nearest_neighbor<<<1,1>>>(out, input, res->getHeight(), res->getWidth(), 4);
+	
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
 	}
 
-	std:getchar();
+
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+	}
+
+	cudaStatus = cudaMemcpy(res->getData(), out, res->getHeight() * res->getWidth() * sizeof(uint32_t), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+	}
+
+	std::cout << "Save new image" << std::endl;
+	res->saveImage();
+
+	std::cout << "Free space" << std::endl;
+	delete org;
+	delete res;
+	ilDeleteImages(1, &handle);
+
+	cudaFree(input);
+	cudaFree(out);
+
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
---
+++ Nearest Neighbor/nearest_neighbor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <IL/il.h>
+#include "image.h"
+
+#define FACTOR 4
+
+void nearest_neighbor(Image *org, Image *res, int factor) {
+    uint32_t *dp = res->getData();
+    uint32_t *sp = org->getData();
+    for(int row = 0; row < res->getHeight(); ++row) {
+        for(int col = 0; col < res->getWidth(); ++col) {
+            dp[(row * res->getWidth() + col)] = sp[((row / factor) * org->getWidth() + (col / factor))];
+        }
+    }
+};
+
+
+void CallNearestNeighbor() {
+
+    // intialize devIL
+    ILuint handle;
+    ilInit();
+    ilEnable(IL_ORIGIN_SET);
+    ilGenImages(1, &handle);
+    ilBindImage(handle);
+    
+    
+    std::cout << "Read images" << std::endl;
+    Image *org = new Image("image.png");
+    Image *res = new Image("image2.png", org->getWidth() * FACTOR, org->getHeight() * FACTOR);
+    
+	std::cout << "Execute nearest neighbor algorithm" << std::endl;
+    nearest_neighbor(org, res, FACTOR);
+    
+	std::cout << "Save new image" << std::endl;
+    res->saveImage();
+
+	std::cout << "Free space" << std::endl;    
+    delete org;
+    delete res;
+    ilDeleteImages(1, &handle);
+}
